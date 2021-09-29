881893cd462d2610bf11a666fc2bb6ae8bab7330
Flaviamol <85742092+Flaviamol@users.noreply.github.com>
2021-09-29 10:10:51 +0200

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return <div className="Footer">Contact us to add more cities </div>;
+}
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Cities from "./Cities";
+import Header from "./Header";
+import Footer from "./Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Challenge 1</h3>
+      <p>
+        Create a new component called Header included in Cities, rendering "The
+        list of cities is:" and include it at the top of all cities.
+        <Header />
+        <Cities />
+        <Footer />
+      </p>
+      <br />
+      <h3>Challenge 2</h3>
+      <p>
+        Create a new component called Footer included in Cities, rendering
+        "Contact us to add more cities" and include it at the Bottom of all
+        cities.
+        <Header />
+        <Cities />
+        <Footer />
+      </p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
